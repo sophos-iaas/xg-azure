5a39b4fcd465e6b63277fd663321333a62eef2a3
David Okeyode <asegunlolu@hotmail.com>
2018-12-20 19:33:35 +0000

    modified to use config export instead of backup

--- artifacts/activate-and-restore-backup-xg.ps1
+++ artifacts/activate-and-restore-backup-xg.ps1
@@ -17,7 +17,7 @@ $secpassword = ConvertTo-SecureString $byolxgpassword -AsPlainText -Force
 $creds = New-Object System.Management.Automation.PSCredential ("admin", $secpassword)
 $session = New-SSHSession -ComputerName $byolxghostname -Credential $creds -AcceptKey -Port $byolxgsshport
 $SSHStream = New-SSHShellStream -SessionId $session.SessionId
-$url="https://$storageaccount.blob.core.windows.net/xgbackup/xgbackup-payg-convert?$sastoken"
+$url="https://$storageaccount.blob.core.windows.net/xgbackup/xgbackup-payg-convert.tar$sastoken"
 
 $licenseblock = @"
 opcode lic_doactivate -s nosync -t json -b '{"serialkey": "$byollicense"}'
@@ -26,10 +26,10 @@ $u2dpatternblock = @"
 opcode u2d_check_pt_updates -ds nosync -t json -b '{"opcodetype": [ "2" ]}'
 "@
 $restoreblock1 = @"
-curl -k -o /tmp/xgbackup-payg-convert "$url"
+curl -k -o /tmp/xgbackup-payg-convert.tar "$url"
 "@
 $restoreblock2 = @"
-opcode upload_restorefile -ds nosync -t json -b '{"restorebackupfile":"/tmp/xgbackup-payg-convert"}'
+opcode upload_text_import_file -s nosync -t json -b '{"importtype":"2","importfile":"/tmp/xgbackup-payg-convert.tar"}'
 "@
 $rebootblock = @"
 opcode soft_reboot -ds nosync
@@ -47,16 +47,11 @@ If ($session.Connected) {
     Start-Sleep -s 5
     $SSHStream.WriteLine("$licenseblock")
     Start-Sleep -s 10
-    $SSHStream.WriteLine("$u2dpatternblock")
-    Start-Sleep -s 300
     $SSHStream.WriteLine("$restoreblock1")
     Start-Sleep -s 5
-    $SSHStream.WriteLine("$u2dpatternblock")
-    Start-Sleep -s 300
     $SSHStream.WriteLine("$restoreblock2")
-    Start-Sleep -s 5
-	$SSHStream.WriteLine("$rebootblock")
-    Start-Sleep -s 60
+    Start-Sleep -s 500
+    Start-Sleep -s 500
     $SSHStream.Read()  
     Remove-SSHSession -SessionId $session.SessionId > $null
 }
--- artifacts/upload-backup-xg.ps1
+++ artifacts/upload-backup-xg.ps1
@@ -15,16 +15,16 @@ $secpassword = ConvertTo-SecureString $paygxgpassword -AsPlainText -Force
 $creds = New-Object System.Management.Automation.PSCredential ("admin", $secpassword)
 $session = New-SSHSession -ComputerName $paygxgip -Credential $creds -AcceptKey -Port $paygxgsshport
 $SSHStream = New-SSHShellStream -SessionId $session.SessionId
-$url="https://$storageaccount.blob.core.windows.net/xgbackup/xgbackup-payg-convert?$sastoken"
+$url="https://$storageaccount.blob.core.windows.net/xgbackup/xgbackup-payg-convert.tar$sastoken"
 $xmsdate = Get-Date
 $xmsdate = $xmsdate.ToUniversalTime()
 $xmsdate = $xmsdate.toString('R')
 
 $backupblock1 = @"
-opcode system_backup_now -ds nosync -t json -b '{"mailloginterval": "-1", "sendtype": "7", "prefix": "current_PAYG_Backup"}'
+opcode apiExport -s nosync -t json -b '{"FullExport":"true"}'
 "@
 $backupblock2 = @"
-curl -k -X PUT -T /tmp/xgbackup-payg-convert -H "x-ms-date: $xmsdate" -H "x-ms-blob-type: BlockBlob" "$url"
+curl -k -X PUT -T /tmp/xgbackup-payg-convert.tar -H "x-ms-date: $xmsdate" -H "x-ms-blob-type: BlockBlob" "$url"
 "@
 If ($session.Connected) {
     Start-Sleep -s 10
@@ -33,8 +33,8 @@ If ($session.Connected) {
     $SSHStream.WriteLine("3")
     Start-Sleep -s 5
     $SSHStream.WriteLine("$backupblock1")
-    Start-Sleep -s 5
-    $SSHStream.WriteLine("cp /sdisk/conf/backupdata/current_PAYG_Backup* /tmp/xgbackup-payg-convert")
+    Start-Sleep -s 600
+    $SSHStream.WriteLine("cp /sdisk/*.tar /tmp/xgbackup-payg-convert.tar")
     Start-Sleep -s 5
 	$SSHStream.WriteLine("$backupblock2")
     Start-Sleep -s 5
